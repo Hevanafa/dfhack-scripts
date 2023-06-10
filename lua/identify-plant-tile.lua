7cc2e9992a6b5eabbe4306ca2743206659077cbb
Hevanafa <dhevansafa4@hotmail.com>
2023-06-11 00:32:05 +0700

    improved plant identification & spawner

--- lua/identify-plant-tile.lua
+++ lua/identify-plant-tile.lua
@@ -1,9 +1,15 @@
 -- By Hevanafa, 08-06-2023
 -- file:///D:/df_47_05_win_b/hack/docs/docs/api/Maps.html?highlight=map+block
 
-local x, y, z = pos2xyz(df.global.cursor)
+local pos = copyall(df.global.cursor)
+local x, y, z = pos2xyz(pos)
 
 -- local block = dfhack.maps.getTileBlock(pos)
+local tile_type = dfhack.maps.getTileType(pos)
+-- print(tile_type)
+
+-- do return end
+
 local plant = dfhack.maps.getPlantAtTile(x, y, z)
 
 if not plant then
@@ -11,5 +17,14 @@ if not plant then
 end
 
 -- see ruby-autogen-win.rb: class Plant
+-- dead_shrub_tile?
 print(plant)
+print(("Tile type: %d"):format(tile_type))
 print(("Material: %d"):format(plant.material))
+print(("Hitpoints: %d"):format(plant.hitpoints))
+print(("Grow counter: %d"):format(plant.grow_counter))
+print(("Damage flags: %s %s %s"):format(
+	plant.damage_flags.is_burning,
+	plant.damage_flags.is_drowning,
+	plant.damage_flags.is_dead
+))
--- lua/spawn-hemp-plants.lua
+++ lua/spawn-hemp-plants.lua
@@ -8,34 +8,56 @@ print(mat_info.index)
 local player_unit = df.global.world.units.active[0]
 assert(player_unit, "Can't find player unit!")
 
+-- see class Tiletype < MemHack::Enum
+-- :ShrubDead
+local dead_shrub_tile_type = 389
+
+local dead_count = 0
+local plant_count = 0
+local count = 0
+
 for _, plant in pairs(df.global.world.plants.all) do
+	local tile_type = dfhack.maps.getTileType(plant.pos)
+
 	if plant.tree_info == nil and
-		not plant.damage_flags.is_dead and
 		plant.material == mat_info.index then
-		-- print(v)
-		-- always ends with :STRUCTURAL but is a number
-		-- print(plant.material)
 
-		-- found in tile-material.lua
-		-- print(dfhack.matinfo.decode(419, plant.material))
+		if tile_type ~= dead_shrub_tile_type then
+			-- print(v)
+			-- always ends with :STRUCTURAL but is a number
+			-- print(plant.material)
 
-		-- See Material info lookup in "Lua API.rst.txt"
-		-- print(dfhack.matinfo.find(plant.material))
-		-- dfhack.matinfo.find("INORGANIC:IRON").material
+			-- found in tile-material.lua
+			-- print(dfhack.matinfo.decode(419, plant.material))
 
-		-- use GetShrubMat(x, y, z) to get the plant material at cursor
+			-- See Material info lookup in "Lua API.rst.txt"
+			-- print(dfhack.matinfo.find(plant.material))
+			-- dfhack.matinfo.find("INORGANIC:IRON").material
 
-		-- stl_vector is based off of class StlVector32
-		-- it's not possible to delete a plant I guess
-		local new_item = df.item.find(
-			dfhack.items.createItem(
-				df.item_type.PLANT, -1,
-				419, plant.material, player_unit
+			-- use GetShrubMat(x, y, z) to get the plant material at cursor
+
+			-- stl_vector is based off of class StlVector32
+			-- it's not possible to delete a plant I guess
+			local new_item = df.item.find(
+				dfhack.items.createItem(
+					df.item_type.PLANT, -1,
+					419, plant.material, player_unit
+				)
 			)
-		)
 
-		new_item.pos = xyz2pos(pos2xyz(plant.pos))
+			new_item.pos = xyz2pos(pos2xyz(plant.pos))
 
-		new_item.stack_size = 1 + math.random(0, 2)
+			new_item.stack_size = 1 + math.random(0, 2)
+
+			plant_count = plant_count + 1
+			count = count + new_item.stack_size
+		else
+			dead_count = dead_count + 1
+		end
 	end
+end
+
+print(("Grown %d hemp plants from %d blocks"):format(count, plant_count))
+if dead_count > 0 then
+	print(("%d dead plants were skipped"):format(dead_count))
 end
