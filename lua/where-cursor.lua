09a761ffe887a6969a8528e0e2e0dbb9ddcc9896
Hevanafa <dhevansafa4@hotmail.com>
2023-06-10 12:52:01 +0700

    changed how to obtain cursor

--- lua/where-cursor.lua
+++ lua/where-cursor.lua
@@ -1,8 +1,10 @@
-local guidm = require("gui.dwarfmode")
-local cursor = guidm.getCursorPos()
+-- local guidm = require("gui.dwarfmode")
+-- local cursor = guidm.getCursorPos()
+local cursor = df.global.cursor
+local x, y, z = pos2xyz(cursor)
 
-if not cursor then
+if not x then
     qerror("Place your cursor somewhere")
 end
 
-print(("%d %d %d"):format(cursor.x, cursor.y, cursor.z))
+print(("%d %d %d"):format(x, y, z))
